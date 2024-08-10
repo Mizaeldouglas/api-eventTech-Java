8c5006f5c8871969a956a1288d037a1270e31121
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-08-10 08:46:23 -0300

    feat: deploy v9

--- src/test/java/com/eventostech/api/ApiApplicationTests.java
+++ src/test/java/com/eventostech/api/ApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.eventostech.api;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.eventostech.api;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
