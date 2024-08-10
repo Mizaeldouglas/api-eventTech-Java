271dcdca52750a921636f44afed1467a3c9ae5a1
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-08-10 09:44:44 -0300

    feat: deploy v12

--- src/main/java/com/eventostech/api/ApiApplication.java
+++ src/main/java/com/eventostech/api/ApiApplication.java
@@ -3,11 +3,10 @@ package com.eventostech.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.eventostech.api")
 public class ApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
 }
+
